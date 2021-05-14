96e9379a89e99669361aa0ae72c57e0a658b10a0
RomanPendyur <51956203+RomanPendyur@users.noreply.github.com>
2021-05-14 22:15:58 +0300

    add gallery and news

--- js/index.js
+++ js/index.js
@@ -2,5 +2,16 @@ $(document).ready(function() {
     $('.header_slider__style').slick({
         arrows: false,
         dots: true,
+        autoplay: true,
+        autoplaySpeed: 1500,
     });
+
+    $('.news_slider__style').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 1500,
+    });
+
 });
